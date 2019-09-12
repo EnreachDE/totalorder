7fa9be2aeceba6893912e7bea24ff8da514c9937
Michael Kubacki <michael.kubacki@swyx.com>
2019-09-12 16:02:21 +0200

    implemented Initialise method in Permission Repo + Test

--- src/to.permissionrepo/to.permissionrepo.test/PermissionRepoTests.cs
+++ src/to.permissionrepo/to.permissionrepo.test/PermissionRepoTests.cs
@@ -8,18 +8,36 @@ using to.contracts.data.result;
 
 namespace to.permissionrepo.test
 {
+    using System.Collections.Generic;
+
+    using contracts;
+
+    using Newtonsoft.Json;
+
     [TestFixture]
     public class PermissionRepoTests
     {
         private PermissionRepo _permissionRepo;
         private const string TestRootDir = "SampleData";
         private const string PermissionsTestJson = "permissionsTest.json";
+        private const string PermissionsMissingTestJson = "permissionsMissingTest.json";
 
         [SetUp]
         public void Setup()
         {
             Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
             _permissionRepo = new PermissionRepo(TestRootDir, PermissionsTestJson);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var path = Path.Combine(TestRootDir, PermissionsMissingTestJson);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
         }
 
         [Test]
@@ -37,6 +55,19 @@ namespace to.permissionrepo.test
             File.Copy(Path.Combine(TestRootDir, "permissionsError.json"), Path.Combine(TestRootDir, PermissionsTestJson), true);
             Action load = () => { var (status, permissions) = _permissionRepo.LoadPermissions(UserRole.Administrator); };
             load.Should().Throw<Exception>();
+        }
+
+        [Test]
+        public void InitialiseSuccessfully()
+        {
+            var path = Path.Combine(TestRootDir, PermissionsMissingTestJson);
+            var permissionRepo = new PermissionRepo(TestRootDir, PermissionsMissingTestJson);
+            var status = permissionRepo.Initialise();
+            status.Should().BeOfType<Success>();
+            File.Exists(path).Should().BeTrue();
+            var fileContent = File.ReadAllText(path);
+            var deserializeObject = JsonConvert.DeserializeObject<Dictionary<UserRole, List<Permission>>>(fileContent);
+            deserializeObject.Should().ContainKeys(UserRole.Administrator, UserRole.Developer, UserRole.Guest, UserRole.ProductOwner);
         }
     }
 }
