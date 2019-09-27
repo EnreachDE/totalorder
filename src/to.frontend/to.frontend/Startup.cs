49175af91f2a880e5d46acc8c1eea25de3e2af3c
Martin Hüser <hueser@gmx.net>
2019-09-27 13:29:24 +0200

    complete dotnet core 3.0 migration
    complete dotnet core 3.0 migration. Work around
    mismatching newtonsoft.json dependency by explicitly
    referencing it in the resp. test projects

--- src/to.frontend/to.frontend/Startup.cs
+++ src/to.frontend/to.frontend/Startup.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
